9938354e949494b31ecb2ceb8f7fe8d06bae7c74
kalasni <kalassni@gmail.com>
2018-09-03 21:02:18 +0200

    Created templates dir and the view index.html

--- crudapp/urls.py
+++ crudapp/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 from .routers import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('article/', TemplateView.as_view(template_name='index.html')),
 ]
 
 """
